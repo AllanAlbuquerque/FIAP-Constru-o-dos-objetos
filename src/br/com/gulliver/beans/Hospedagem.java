45f80287d40d6f5e6be1f51d3f392c9fcf71e125
otavio augusto <otavio95123@gmail.com>
2022-08-14 18:26:26 -0300

    concluido restaurante e hospedagem

--- src/br/com/gulliver/beans/Hospedagem.java
+++ src/br/com/gulliver/beans/Hospedagem.java
@@ -1,5 +1,62 @@
 package br.com.gulliver.beans;
 
 public class Hospedagem {
+	private int id;
+	private int titulo;
+	private int descricao;
+	private Endereco[] endereco;
+	private Imagem[] imagem;
+	
+	public Hospedagem() {
+		super();
+	}
+	
+	public Hospedagem(int id, int titulo, int descricao, Endereco[] endereco, Imagem[] imagem) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.endereco = endereco;
+		this.imagem = imagem;
+	}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(int titulo) {
+		this.titulo = titulo;
+	}
+
+	public Endereco[] getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco[] endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getDecricao() {
+		return descricao;
+	}
+
+	public void setDescricao(int descricao) {
+		this.descricao = descricao;
+	}
+
+	public Imagem[] getImagem() {
+		return imagem;
+	}
+
+	public void setImagens(Imagem[] imagem) {
+		this.imagem = imagem;
+	}
 }
--- src/br/com/gulliver/beans/Restaurante.java
+++ src/br/com/gulliver/beans/Restaurante.java
@@ -1,5 +1,65 @@
 package br.com.gulliver.beans;
 
 public class Restaurante {
+	private int id;
+	private int titulo;
+	private int descricao;
+	private Endereco[] endereco;
+	private Imagem[] imagem;
+	
+	public Restaurante()  {
+		super();
+	}
+
+      public Restaurante(int id, int titulo, int descricao,Endereco[] endereco,  Imagem[] imagem) {
+	super();
+	this.id = id;
+	this.titulo = titulo;
+	this.descricao = descricao;
+	this.endereco = endereco;
+	this.imagem = imagem;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public int getTitulo() {
+	return titulo;
+}
+
+public void setTitulo(int titulo) {
+	this.titulo = titulo;
+}
+
+
+public int getDecricao() {
+	return descricao;
+}
+
+public void setDescricao(int descricao) {
+	this.descricao = descricao;
+}
+
+public Endereco[] getEndereco() {
+	return endereco;
+}
+
+public void setEndereco(Endereco[] endereco) {
+	this.endereco = endereco;
+}
+
+public Imagem[] getImagem() {
+	return imagem;
+}
+
+public void setImagens(Imagem[] imagem) {
+	this.imagem = imagem;
+}
+
 
 }
