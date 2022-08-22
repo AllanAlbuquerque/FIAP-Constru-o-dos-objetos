d6ebc5ef2c81f9394e92705b9b2bb071f68ec1ce
caiomyrapereira <caiomyrapereira@hotmail.com>
2022-08-22 19:01:01 -0300

    atualizar um novo inicio.

--- src/br/com/gulliver/inicio/Inicio.java
+++ src/br/com/gulliver/inicio/Inicio.java
@@ -1,5 +1,8 @@
 package br.com.gulliver.inicio;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import br.com.gulliver.beans.Atividade;
 import br.com.gulliver.beans.Avaliacao;
 import br.com.gulliver.beans.Contato;
@@ -14,11 +17,6 @@ import br.com.gulliver.beans.Usuario;
 public class Inicio {
 
 	public static void main(String[] args) {
-		/* Criar imagem exemplo */
-		Imagem imagem = new Imagem(1, "/src/img/restaurante.jpg", "Restaurante abc");
-		imagem.cadastrarImagem();
-		
-		/* ******************************* */
 		
 		/* Criar endereco exemplo */
 		Endereco endereco = new Endereco();
@@ -31,27 +29,45 @@ public class Inicio {
 		endereco.setPais("Brasil");
 		endereco.setCidade("Sao Paulo");
 		
-		/* ******************************* */
-		
-		/* Criar mensagem exemplo */
-		Mensagem mensagem = new Mensagem(1, "Melhor experiencia da minha vida");
+		System.out.println( endereco.toString() );
 		
 		/* ******************************* */
 		
-		/* Criar Avaliacao exemplo */
-		/* Uma avaliacao pode ter varias mensagens entao criamos um array */
-		Mensagem mensagens[] = new Mensagem[1];
-		mensagens[0] = mensagem; //Primeiro item é a nossa mensagem.
+		/* Criar atividade exemplo */
+		Atividade atividade = new Atividade();
+		atividade.setId(1);
+		atividade.setTitulo("Excursao");
+		atividade.setDescricao("Excursao do ponta A ao ponto B");
+		atividade.setEndereco(endereco);
 		
-		//A nova avaliacao recebe nossa lista de mensagem
-		Avaliacao avaliacao = new Avaliacao(1, 5, mensagens);
+		System.out.println( atividade );
 		
 		/* ******************************* */
 		
-		/* Criar Restaurante exemplo */
-		/* Um restaurante pode ter diversas imagens */
-		Imagem imagensRestaurante[] = new Imagem[1];
-		imagensRestaurante[0] = imagem;
+		/* Criar Contato exemplo */
+		Contato contato = new Contato(1, 1199999999, "abc", "bcd", "efg");
+		
+		System.out.println(contato);
+		/* ******************************* */
+				
+		/* Criar imagem para atividade, restaurante e hospedagem exemplo */
+		Imagem imagemAtividade01 = new Imagem(1, "/src/img/atividade1.jpg", "Atividade abc");
+		imagemAtividade01.cadastrarImagem();
+		Imagem imagemAtividade02 = new Imagem(2, "/src/img/atividade2.jpg", "Atividade abc");
+		imagemAtividade02.cadastrarImagem();
+		
+		Imagem imagemRestaurante = new Imagem(1, "/src/img/restaurante.jpg", "Restaurante abc");
+		imagemRestaurante.cadastrarImagem();
+		
+		Imagem imagemHospedagem = new Imagem(2, "/src/img/hospedagem.jpg", "Hospedagem abc");
+		imagemHospedagem.cadastrarImagem();
+		
+		/* ******************************* */
+		
+		/* Criar Restaurante  exemplo */
+		/* Um restaurante pode ter diversas imagens  com um endereco*/
+		ArrayList<Imagem> imagensRestaurante = new ArrayList<Imagem>();
+		imagensRestaurante.add(imagemRestaurante);
 		
 		Restaurante restaurante = new Restaurante(1,
 				"Restaurante A",
@@ -59,10 +75,14 @@ public class Inicio {
 				endereco,
 				imagensRestaurante);
 		
+		System.out.println(restaurante);
+		
+		/* ******************************* */
+		
 		/* Criar Hospedagem exemplo */
-		/* Uma hospefagem pode ter diversas imagens */
-		Imagem imagensHospedagem[] = new Imagem[1];
-		imagensHospedagem[0] = imagem;
+		/* Uma hospefagem pode ter diversas imagens  com um endereco*/
+		ArrayList<Imagem> imagensHospedagem = new ArrayList<Imagem>();
+		imagensHospedagem.add(imagemHospedagem);
 		
 		Hospedagem hospedagem = new Hospedagem(1,
 				"Hospedagem A",
@@ -70,45 +90,112 @@ public class Inicio {
 				endereco,
 				imagensHospedagem);
 		
-		/* Criar Atividade exemplo */
-		/* Uma atividade pode ter varias imagens, restaurantes, 
-		 * avaliacoes e hospedagens entao criamos um array */
-		Imagem imagens[] = new Imagem[1];
-		imagens[0] = imagem;
+		System.out.println(hospedagem);
 		
-		Avaliacao avaliacoes[] = new Avaliacao[1];
-		avaliacoes[0] = avaliacao;
+		/* ******************************* */
 		
-		Restaurante restaurantes[] = new Restaurante[1];
-		restaurantes[0] = restaurante;
+		/* Criar mensagem exemplo */
+		Mensagem mensagem1= new Mensagem(1, "Melhor experiencia da minha vida");
+		Mensagem mensagem2 = new Mensagem(2, "seila man. Não faço ideias.");
+		System.out.println(mensagem1);
+		System.out.println(mensagem2);
 		
-		Hospedagem hospedagens[] = new Hospedagem[1];
-		hospedagens[0] = hospedagem;
+		/* ******************************* */
+
+		/* Criar Avaliacao exemplo */
+		/* Uma avaliacao pode ter varias mensagens entao criamos um array */
+		ArrayList<Mensagem> mensagens = new ArrayList<Mensagem>();
+		mensagens.add( mensagem1); //Primeiro item é a nossa mensagem.
+		mensagens.add( mensagem2); //segunda item é a nossa mensagem.
 		
-		Atividade atividade = new Atividade();
-		atividade.setId(1);
-		atividade.setTitulo("Excursao");
-		atividade.setDescricao("Excursao do ponta A ao ponto B");
-		atividade.setEndereco(endereco);
-		atividade.setRestaurante(restaurantes);
-		atividade.setHospedagem(hospedagens);
-		atividade.setImagem(imagens);
-		atividade.setAvaliacao(avaliacoes);
+		System.out.println(mensagens);
 		
-		/* Criar Contato exemplo */
-		Contato contato = new Contato(1, 1199999999, "abc", "bcd", "efg");
+		/* ******************************* */
+		
+		//A nova avaliacao recebe nossa lista de mensagem
+		Avaliacao avaliacao = new Avaliacao(1, 5, mensagens);
+		
+		System.out.println(avaliacao);
+		
+		/* ******************************* */
+		
+		/* avaliacoes, restaurantes e hospedagens entao criamos um array */
+		
+		HashSet<Avaliacao> avaliacoes = new HashSet<Avaliacao>();
+		avaliacoes.add(avaliacao);
+		avaliacoes.add(avaliacao);
+		avaliacoes.add(avaliacao);
+		avaliacoes.add(avaliacao);
+		
+		HashSet<Restaurante> restaurantes = new HashSet<Restaurante>();
+		restaurantes.add(restaurante);
+		restaurantes.add(restaurante);
+		restaurantes.add(restaurante);
+		
+		
+		HashSet<Hospedagem> hospedagens = new HashSet<Hospedagem>();
+		hospedagens.add(hospedagem);
+		hospedagens.add(hospedagem);
+		hospedagens.add(hospedagem);
+		/* ******************************* */
+		
+		/* Uma atividade pode ter varias imagens, restaurantes e avaliacao, */
+		
+		HashSet<Imagem> imagens = new HashSet<Imagem>();
+		imagens.add(imagemAtividade01);
+		imagens.add(imagemAtividade01);
+		imagens.add(imagemAtividade01);
+		imagens.add(imagemAtividade02);
+		imagens.add(imagemAtividade02);
+		imagens.add(imagemAtividade02);
+		
+		System.out.println(imagens);
+		
+		for(Imagem img:imagens) {
+			atividade.setImagem(img);
+		}
+		
+		System.out.println(atividade);
+		
+		for(Restaurante r:restaurantes) {
+			atividade.setRestaurante(r);
+		}
+		
+		System.out.println(atividade);
+		
+		for(Hospedagem h:hospedagens) {
+			atividade.setHospedagem(h);
+		}
+		
+		System.out.println(atividade);
+		
+		
+		for(Avaliacao v:avaliacoes) {
+			atividade.setAvaliacao(v);
+		}
+		
+		System.out.println(atividade);
+		
+
+		/* ******************************* */
 		
 		/* Criar Perfil exemplo */
 		/* Um perfil pode ter varias atividades entao criamos um array */
-		Atividade atividades[] = new Atividade[1];
-		atividades[0] = atividade;
+		ArrayList<Atividade> atividades = new ArrayList<Atividade>();
+		atividades.add(atividade);
 		
-		Perfil perfil = new Perfil(1,
+		Perfil perfil = new Perfil(
+				1,
 				"nomeUser",
 				"breve descricao",
 				"a@email.com",
 				contato,
-				atividades);
+				atividades
+		);
+		
+		System.out.println( perfil );
+		
+		/* ******************************* */
 		
 		/* Criar Usuario exemplo */
 		Usuario usuario = new Usuario(1,
@@ -117,6 +204,10 @@ public class Inicio {
 				perfil);
 		
 		usuario.logarUsuario();
+		
+		System.out.println( usuario );
+	
+		
 	}
 
 }
